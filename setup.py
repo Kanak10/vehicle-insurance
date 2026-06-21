ec8fe347895fb3d0cb464c0757b94f23793baec5
Kanak Gupta <kanakgupta814@gmail.com>
2026-06-21 22:16:30 +0530

    change setup project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="src",
+    name="vehicle-insurance",
     version="0.0.1",
     author="Kanak gupta",
     author_email="kanakgupta814@gmail.com",
